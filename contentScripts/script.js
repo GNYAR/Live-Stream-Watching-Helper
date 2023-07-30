2bb7322751560186e2e1ef9455860546ac611856
GNYAR <s901121s901121@gmail.com>
2023-07-30 21:37:42 +0800

    fix(videohider): correct the display when directing back

--- contentScripts/script.js
+++ contentScripts/script.js
@@ -42,7 +42,10 @@ document.arrive(".emote-card h4", function () {
 });
 
 // VideoHider
-document.arrive(".user-avatar-card__live", { onceOnly: true }, function () {
+document.arrive(".user-avatar-card__live", function () {
+  if ($("#videoBtn").length) return;
+
+  log("VideoHider on");
   const EYE_HTML =
     '<svg width="20" height="20" fill="#FFF" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" /></svg>';
   const EYE_OFF_HTML =
@@ -76,4 +79,10 @@ document.arrive(".user-avatar-card__live", { onceOnly: true }, function () {
     });
 
   $(".player-controls__right-control-group").prepend(btn);
+});
+
+document.leave("#videoBtn", function () {
+  log("VideoHider off");
+  $("#hiddenTxt").remove();
+  $("video").show();
 });
